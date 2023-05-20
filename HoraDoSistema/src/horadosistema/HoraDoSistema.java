083ff46aed6a051489d7e0f4cb25747a2153b189
Michaell Cabral Analista de Sistemas <88794323+MichaellCabral@users.noreply.github.com>
2023-05-20 15:04:39 -0300

    update
    aula 05 poo netbeans
    hora do sistema java
    idioma do sistema java

---
+++ HoraDoSistema/src/horadosistema/HoraDoSistema.java
@@ -0,0 +1,16 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+
+
+    public static void main(String[] args) {
+        //new cria um novo objeto que no caso Ã© o relogio
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é");
+        System.out.println(relogio.toString());
+        
+}
+    
+}
